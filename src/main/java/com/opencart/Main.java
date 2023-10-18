4278154e92eaac7f4275f13c30765c2d9c89ba24
Mariana <bulargamariana@mail.ru>
2023-10-18 17:03:27 +0300

    Inmplemented the DriverManager and configure the Selenium depencies

--- src/main/java/com/opencart/Main.java
+++ src/main/java/com/opencart/Main.java
@@ -1,7 +1,9 @@
 package com.opencart;
 
 import com.opencart.managers.DriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -15,10 +17,47 @@ public class Main {
 
         // New Window Code
         driver.switchTo().newWindow(WindowType.TAB);
-        driver.get("https://www.google.com/");
-        Thread.sleep( 2000);
+        driver.get("https://opencart.antropy.co.uk/");
+        Thread.sleep( 5000);
+
+        WebElement myAccountIcon = driver.findElement(By.xpath("//i[@class='fa fa-user']"));
+
+        myAccountIcon.click();
+
+        WebElement registerOption = driver.findElement(By.xpath("//a[normalize-space()='Register']"));
+
+        registerOption.click();
+
+        System.out.println(driver.getCurrentUrl());
+
+WebElement firstNameInput  =  driver.findElement(By.id("input- firstname"));
+firstNameInput.sendKeys("Victor");
+
+WebElement lastNameInput = driver.findElement (By.cssSelector("# input-lastname"));
+lastNameInput.sendKeys("Tescu");
+
+WebElement emailInput = driver.findElement(By.cssSelector("#input-email"));
+emailInput.sendKeys("boss007@gmail.com");
+
+WebElement phoneInput = driver.findElement(By.cssSelector("#input-telephone"));
+phoneInput.sendKeys("07985547");
+
+WebElement passwordInput = driver.findElement(By.cssSelector("#input-password"));
+passwordInput.sendKeys("Parola123!");
+
+WebElement confirmPasswprdInput = driver.findElement(By.cssSelector("#input-confirm"));
+confirmPasswprdInput.sendKeys("Parola123!");
+
+WebElement  termsAndConditionCheckbox = driver.findElement(By.xpath("//input[@name='agree']"));
+termsAndConditionCheckbox.click();
+
+WebElement registerBtn = driver.findElement(By.xpath("//input[@value='Continue']"));
+registerBtn.click();
+        Thread.sleep( 5000);
+
         System.out.println(driver.getTitle());
         driver.close();
+
         driver.switchTo().window(currentWindoname);
         driver.get("https://tekwill.md/");
         Thread.sleep( 1000);
